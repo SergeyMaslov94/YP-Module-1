a915a7d46df272252eaa161a03a08fdbeba0c177
Маслов Сергей Олегович <sergey.maslov@orspo>
2022-11-22 18:06:26 +0300

    Устройство unordered_map и unordered_set

--- Sprint_9/Topic_1/lesson_3.hpp
+++ Sprint_9/Topic_1/lesson_3.hpp
@@ -40,10 +40,27 @@ private:
     int region_;
 };
 
+//======================================================
+
 ostream& operator<<(ostream& out, VehiclePlate plate) {
     out << plate.ToString();
     return out;
 }
+
+inline bool operator==(const VehiclePlate& lhs, const VehiclePlate& rhs) 
+{
+    if (lhs.Hash() == rhs.Hash())
+    {
+        auto str1 = lhs.ToString();
+        auto str2 = rhs.ToString();
+        return str1 == str2;
+    }
+    else
+    {
+        return false;
+    }
+}
+//======================================================
 
 template <typename T>
 class HashableContainer {
@@ -57,15 +74,24 @@ public:
             elements_.resize(index * 2 + 1);
         }
 
-        elements_[index] = move(elem);
+        auto& element = elements_.at(index);
+        auto it = std::find(element.begin(), element.end(), elem);
+        
+        if (it == element.end())
+        {
+            element.push_back(move(elem));
+        }
     }
 
     void PrintAll(ostream& out) const {
-        for (auto& e : elements_) {
-            if (!e.has_value()) {
+        for (auto& element : elements_) {
+            if (element.empty()) {
                 continue;
             }
-            out << e.value() << endl;
+            for (auto& value : element)
+            {
+                out << value << endl;
+            }
         }
     }
 
@@ -74,29 +100,27 @@ public:
     }
 
 private:
-    vector<vector<T>> elements_;
+    vector<std::vector<T>> elements_;
 };
 
 int lesson_3() {
     HashableContainer<VehiclePlate> plate_base;
 
-    plate_base.Insert({ 'B', 'H', 5, 'E', 99 });
-    plate_base.Insert({ 'B', 'H', 5, 'E', 99 });
-
-
-    plate_base.Insert({ 'B', 'H', 840, 'E', 99 });
-    plate_base.Insert({ 'O', 'K', 942, 'K', 78 });
-    plate_base.Insert({ 'O', 'K', 942, 'K', 78 });
-    plate_base.Insert({ 'O', 'K', 942, 'K', 78 });
-    plate_base.Insert({ 'O', 'K', 942, 'K', 78 });
-    plate_base.Insert({ 'H', 'E', 968, 'C', 79 });
-    plate_base.Insert({ 'T', 'A', 326, 'X', 83 });
-    plate_base.Insert({ 'H', 'H', 831, 'P', 116 });
-    plate_base.Insert({ 'P', 'M', 884, 'K', 23 });
-    plate_base.Insert({ 'O', 'C', 34, 'P', 24 });
-    plate_base.Insert({ 'M', 'Y', 831, 'M', 43 });
-    plate_base.Insert({ 'K', 'T', 478, 'P', 49 });
-    plate_base.Insert({ 'X', 'P', 850, 'A', 50 });
+    plate_base.Insert({ 'B','H', 840, 'E', 99 });
+    plate_base.Insert({ 'O','K', 942, 'K', 78 });
+    plate_base.Insert({ 'O','K', 942, 'K', 78 });
+    plate_base.Insert({ 'O','K', 942, 'K', 78 });
+    plate_base.Insert({ 'O','K', 942, 'K', 78 });
+    plate_base.Insert({ 'H','E', 968, 'C', 79 });
+    plate_base.Insert({ 'T','A', 326, 'X', 83 });
+    plate_base.Insert({ 'H','H', 831, 'P', 116 });
+    plate_base.Insert({ 'A','P', 831, 'Y', 99 });
+    plate_base.Insert({ 'P','M', 884, 'K', 23 });
+    plate_base.Insert({ 'O','C', 34, 'P', 24 });
+    plate_base.Insert({ 'M','Y', 831, 'M', 43 });
+    plate_base.Insert({ 'B','P', 831, 'M', 79 });
+    plate_base.Insert({ 'K','T', 478, 'P', 49 });
+    plate_base.Insert({ 'X','P', 850, 'A', 50 });
 
     plate_base.PrintAll(cout);
 
--- Sprint_9/sprint_9.cpp
+++ Sprint_9/sprint_9.cpp
@@ -1,13 +1,15 @@
-#include "Topic_1/lesson_1.hpp"
-#include "Topic_1/lesson_2.hpp"
-#include "Topic_1/lesson_3.hpp"
+//#include "Topic_1/lesson_1.hpp"
+//#include "Topic_1/lesson_2.hpp"
+//#include "Topic_1/lesson_3.hpp"
+#include "Topic_1/lesson_4.hpp"
 //============================================
 int main() {
 	//============================================================================================================================
 	// ТЕМА 1: АССОЦИАТИВНЫЕ КОНТЕЙНЕРЫ
-	//lesson_1(); // map, unordered_map 
-	//lesson_2(); // считаем купюры
-	lesson_3(); // хэш-функция
+	//lesson_1();	// map, unordered_map 
+	//lesson_2();	// считаем купюры
+	//lesson_3();	// хэш-функция
+	lesson_4();		// устройство unordered_map и unordered_set
 
 	return 0;
 }
